af4950826b5323c95439ac9b3d24af204ca50474
alexotalvaro <alejandromilles@outlook.com>
2022-11-29 13:58:06 +0100

    29/11/2022 Modificacion index.php

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 
 
     <head>
-        <title>EJERCICIOS TEMA 4</title>
+        <title>DESPLIEGUE DE APLICACIONES WEB</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Tipografía -->
         <link href="https://fonts.googleapis.com/css2?family=Chela+One&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Lobster&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
+
         <link href="https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&display=swap&" rel="stylesheet">
 
 
@@ -25,7 +24,7 @@
             h1 {
 
                 text-align: center;
-                
+
                 color: black;
                 font-family: 'Hachi Maru Pop', cursive;
                 text-shadow: 4px 4px 4px #777,
@@ -61,33 +60,32 @@
                 top: 25px;
                 right: 200px;
             }
-
-
-            td img {
-                Width: 15px;
-            }
         </style>
 
     </head>
 
     <body>
-        
+
         <div class="contenedor">
             <h1>DESPLIEGUE DE APLICACIONES WEB</h1  
         </div>
 
         <footer>
-            <a href="/../index.php" style="text-decoration:none">
-                <img src="../doc/home.png" alt="inicio" onmouseover="this.width = 50;this.height = 50;"
-                     onmouseout="this.width = 39;
-                             this.height = 39;" width="39" title="inicio" />
-            </a>
+
             <a href="/doc/CV.pdf" target="_blank" style="text-decoration: none">
                 <img src="/doc/cv-logo.png" alt="curriculum" onmouseover="this.width = 50;" onmouseout="this.width = 39;"
                      width="39" title="curriculum vitae" class="icono_cv" />
             </a>
-
+            <a href="/../index.php" style="text-decoration:none">
+                <img src="../doc/home.png" alt="inicio" onmouseover="this.width = 50;this.height = 50;" onmouseout="this.width = 39; this.height = 39;" width="39" title="inicio" />
+            </a>
+            
             © Alejandro Otálvaro Marulanda
+
+            <a href="https://github.com/alexotalvaro" target="_blank" style="text-decoration: none">
+                <img src="/doc/git.png" alt="git" onmouseover="this.width = 50;" onmouseout="this.width = 39;"
+                     width="39" title="GIT" />
+            </a>
 
             <a href="http://www.linkedin.com/in/alejandro-otálvaro-marulanda/" target="_blank"
                style="text-decoration: none">
