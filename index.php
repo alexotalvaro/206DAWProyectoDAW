334e459c2cab82b48fc87e4926d43b9aea112f0b
alexotalvaro <alejandromilles@outlook.com>
2022-12-14 10:52:12 +0100

    14/12/2022
    estado final todo bien

--- index.php
+++ index.php
@@ -9,66 +9,41 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Tipografía -->
         <link href="https://fonts.googleapis.com/css2?family=Chela+One&display=swap" rel="stylesheet">
-
         <link href="https://fonts.googleapis.com/css2?family=Hachi+Maru+Pop&display=swap&" rel="stylesheet">
+        <link href="webroot/css/estilos.css" rel="stylesheet" type="text/css">
 
-
-
-        <style>
-            body {
-                background: gainsboro;
-
-
-            }
-
-            h1 {
-
-                text-align: center;
-
-                color: black;
-                font-family: 'Hachi Maru Pop', cursive;
-                text-shadow: 4px 4px 4px #777,
-                    8px 8px 8px #145;
-
-
-            }
-
-
-            footer {
-                background-color: grey;
-                position: fixed;
-                width: 100%;
-                height: 5vh;
-                font-size: 1em;
-                font-family: 'Chela One', cursive;
-                text-align: center;
-                padding-top: 20px;
-                padding-bottom: 30px;
-                bottom: 0;
-                left: 0px;
-            }
-
-
-            .icono_cv {
-                position: absolute;
-                top: 25px;
-                left: 100px;
-            }
-
-            .icono_link {
-                position: absolute;
-                top: 25px;
-                right: 200px;
-            }
-        </style>
 
     </head>
 
     <body>
 
-        <div class="contenedor">
-            <h1>DESPLIEGUE DE APLICACIONES WEB</h1  
-        </div>
+        <main class="contenedor">
+            <h1>DESPLIEGUE DE APLICACIONES WEB</h1>
+
+            <div class="gallery">
+                <div class="thumbnail" id="oculto"> <a href="#"><img src="./doc/fondopdf.png" alt="" width="2000" class="cards"/></a>
+                    <h4>TITLE</h4>
+                    <p class="tag">HTML, CSS, JS, WordPress</p>
+                    <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+               <div class="thumbnail"> <a href="./doc/tema1daw.pdf" target="_blank"><img src="./doc/fondopdf.png" alt="" width="2000" class="cards"/></a>
+                    <h4>TEMA 1</h4>
+                    <p class="tag"></p>
+                    <p class="text_column">PDF con la resolución de los ejercicios del tema 1.</p>
+                </div>
+                <div class="thumbnail"> <a href="./doc/tema2daw.pdf" target="_blank"><img src="./doc/fondopdf.png" alt="" width="2000" class="cards"/></a>
+                    <h4>TEMA 2</h4>
+                    <p class="tag"></p>
+                    <p class="text_column">PDF con la resolución de los ejercicios del tema 2.</p>
+                </div>
+                <div class="thumbnail" id="oculto"> <a href="#"><img src="./doc/fondopdf.png" alt="" width="2000" class="cards"/></a>
+                    <h4>TITLE</h4>
+                    <p class="tag">HTML, CSS, JS, WordPress</p>
+                    <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+
+        </main>
 
         <footer>
 
@@ -79,11 +54,11 @@
             <a href="/../index.php" style="text-decoration:none">
                 <img src="../doc/home.png" alt="inicio" onmouseover="this.width = 50;this.height = 50;" onmouseout="this.width = 39; this.height = 39;" width="39" title="inicio" />
             </a>
-            
+
             © Alejandro Otálvaro Marulanda
 
             <a href="https://github.com/alexotalvaro" target="_blank" style="text-decoration: none">
-                <img src="/doc/git.png" alt="git" onmouseover="this.width = 50;" onmouseout="this.width = 39;"
+                <img src="./doc/git.png" alt="git" onmouseover="this.width = 50;" onmouseout="this.width = 39;"
                      width="39" title="GIT" />
             </a>
 
